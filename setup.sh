8bd3345277d977742ad2d1e19743368b1ba7c9ec
Andrew Osheroff <andrewosh@gmail.com>
2015-08-17 17:35:46 -0400

    Modified setup.sh a bit more

--- setup.sh
+++ setup.sh
@@ -4,6 +4,11 @@
 apt-get update -y
 apt-get install -y git vim python-pip
 
+# setup gcloud
+curl https://sdk.cloud.google.com | bash
+gcloud preview
+gcloud auth login
+
 # get/install anaconda
 wget https://3230d63b5fc54e62148e-c95ac804525aac4b6dba79b00b39d1d3.ssl.cf1.rackcdn.com/Anaconda-2.3.0-Linux-x86_64.sh
 bash Anaconda-2.3.0-Linux-x86_64.sh
@@ -20,7 +25,14 @@ git clone https://www.github.com/binder-project/binder
 cd binder/
 make install
 
-# configure environment variables
-echo "BINDER_HOME=~/binder" >> ~/.bashrc
-echo "PATH=$PATH:$BINDER_HOME/bin" >> ~/.bashrc
-echo "PYTHONPATH=$PYTHONPATH:$BINDER_HOME" >> ~/.bashrc
+# configure binder environment variables
+echo "export BINDER_HOME=~/binder" >> ~/.bashrc
+echo "export PATH=$PATH:$BINDER_HOME/bin" >> ~/.bashrc
+echo "export PYTHONPATH=$PYTHONPATH:$BINDER_HOME" >> ~/.bashrc
+
+# configure kubernetes environment variables
+echo "export KUBERNETES_HOME=~/kubernetes" >> ~/.bashrc
+echo "export PATH=$PATH:/usr/local/go/bin" >> ~/.bashrc
+echo "export PATH=$PATH:$KUBERNETES_HOME/cluster" >> ~/.bashrc
+
+
