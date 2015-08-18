5777a1c4b137a336b28c7b529d851bbc4909d2d3
Andrew Osheroff <andrewosh@gmail.com>
2015-08-17 20:41:49 -0400

    Small settings change

--- binder/settings.py
+++ binder/settings.py
@@ -4,6 +4,10 @@ import logging
 if "BINDER_HOME" not in os.environ:
     raise Exception("BINDER_HOME environment variable must be set")
 
+provider = os.environ.get("KUBERNETES_PROVIDER")
+if not provider:
+    os.environ["KUBERNETES_PROVIDER"] = "gce"
+
 ROOT = os.environ["BINDER_HOME"]
 DOCKER_HUB_USER = "andrewosh"
 REGISTRY_NAME = "gcr.io/generic-notebooks"
