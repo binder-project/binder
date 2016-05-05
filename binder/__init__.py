ca138eed3233cbf5d4ff8c7bed5903712ac03c2b
freeman <the.freeman.lab@gmail.com>
2016-05-05 16:22:47 +0200

    remove legacy files

--- binder/__init__.py
+++
@@ -1,8 +0,0 @@
-import signal
-from binder.log import LoggerClient
-
-def sig_handler(sig, frame):
-    LoggerClient.getInstance().stop()
-
-signal.signal(signal.SIGTERM, sig_handler)
-signal.signal(signal.SIGINT, sig_handler)
--- services/spark/1.4.1/images/master/start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-start-master.sh
-
-# start-master launches in the background, so this foreground thread should spin
-while [ 1 ]
-do
-  sleep 5
-done
